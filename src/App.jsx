d5ef48d2c48cdf103e967221afcb86992b9f9c1b
Harsha Anuga <harshaanuga358@gmail.com>
2025-04-05 23:24:00 +0530

    wrapped the application in Recoil root

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { RecoilRoot,useRecoilValue } from 'recoil'
+import { networkAtom } from './assets/atoms'
 
 function App() {
-  const Networknotificationcount=useRecoilValue(networkAtom)
+  return<RecoilRoot>
+    <MainApp />
+  </RecoilRoot>
+}
+function MainApp() {
+  const networkNotificationCount = useRecoilValue(networkAtom)
 
 
   return (
     <>
       <button>Home</button>
 
-      <button> My Network</button>
+      <button> My Network({networkNotificationCount>=100 ? "99+": networkNotificationCount})</button>
       <button> Messaging</button>
       <button>Notification</button>
       
       <button>Me</button>
     </>
   )
-}
 
+}
 export default App
